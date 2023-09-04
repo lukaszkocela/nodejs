1e34ce293431699d4120dda8c1dca2e7142c3905
Łukasz Kocela <lukaszkocela@gmail.com>
2023-09-04 15:50:24 +0200

    1stver

--- server.js
+++ server.js
@@ -1,5 +1,51 @@
-const app = require('./app')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const routerApi = require("./api");
+app.use("/api", routerApi);
+
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: "Use api on routes: /api/contacts",
+    data: "Not found",
+  });
+});
+
+app.use((err, _, res, __) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    dbName: "db-contacts",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Database connection error: ${err.message}`);
+    process.exit(1);
+  });
---
+++ service/index.js
@@ -0,0 +1,34 @@
+const Contact = require("./schemas/contact");
+
+const getAllContacts = async () => {
+  return Contact.find();
+};
+
+const getContactById = (id) => {
+  return Contact.findOne({ _id: id });
+};
+
+const createContact = ({ name, email, phone }) => {
+  return Contact.create({ name, email, phone });
+};
+
+const updateContact = (id, fields) => {
+  return Contact.findByIdAndUpdate({ _id: id }, fields, { new: true });
+};
+
+const updateStatusContact = (id, body) => {
+  return Contact.findByIdAndUpdate(id, body);
+};
+
+const removeContact = (id) => {
+  return Contact.findByIdAndRemove({ _id: id });
+};
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  removeContact,
+  updateStatusContact,
+};
